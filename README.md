4596525e571d9f54fa627d2ee7cecd54ecf448f0
Alfredo <alfredinho303@gmail.com>
2025-10-17 17:14:59 -0700

    Commit for TicTacToe game

---
+++ README.md
@@ -0,0 +1,29 @@
+# TicTacToe branch by Alfredo Barragan
+
+THIS WAS DONE IN MACOS
+This commit includes a working TicTacToe game with correct outputs however code redability is something I still need.
+
+TODO LIST:
+
+1.Program Builds Successfully (After all implementation is done)
+    -DONE: Able to build and run with no errors after all required implementation.
+
+2.Game works properly: allows interactive turn by turn play
+    -DONE: Able to place pieces at all 9 possible places with a second player also (NOT AI as of current submission)
+
+3.Game works properly: detects all win/draw conditions
+    -Working property. 
+
+    -NEED HELP WITH CODE REDABILITY ON THIS.
+
+4.Game works properly: allows reset
+    -DONE: Reset Game button erases all filled squares and allows to play another game.
+
+5.Code readability
+    -INCOMPLETE: Although I am happy with getting it to run there is definitely some stuff that I
+    am unaware of that will help with readability.
+
+6.Required comments describing implementation
+    -DONE: added comments inside fuction and ontop of function describing how i implemented them
+
+7.Extra credit: a simple AI
--- classes/TicTacToe.cpp
+++ classes/TicTacToe.cpp
@@ -58,6 +58,22 @@ void TicTacToe::setUpBoard()
     // then we need to setup our 3x3 array in _grid with the correct position of the square, and load the "square.png" sprite for each square
     // we will use the initHolder function on each square to do this
     // finally we should call startGame to get everything going
+
+    //set # of players and rows in total
+    setNumberOfPlayers(2);
+    _gameOptions.rowX = 3;
+    _gameOptions.rowY = 3;
+    
+    //looping through from left-to-right & top-to-bottom
+    //load all needed squares (9) spaced out by multiplying by 100
+    for(int i = 0; i < _gameOptions.rowX; ++i) {
+        for (int j = 0; j < _gameOptions.rowY; ++j) {
+            _grid[j][i].initHolder(ImVec2(j * 100,i * 100), "square.png", j, i);
+        }
+    }
+
+    //call startGame() after creating the gameBoard
+    startGame();
 }
 
 //
@@ -68,10 +84,12 @@ bool TicTacToe::actionForEmptyHolder(BitHolder *holder)
     // 1) Guard clause: if holder is nullptr, fail fast.
     //    (Beginner hint: always check pointers before using them.)
     //    if (!holder) return false;
+    if(!holder) return false;
 
     // 2) Is it actually empty?
     //    Ask the holder for its current Bit using the bit() function.
     //    If there is already a Bit in this holder, return false.
+    if(holder->bit()) return false;
 
     // 3) Place the current player's piece on this holder:
     //    - Figure out whose turn it is (getCurrentPlayer()->playerNumber()).
@@ -79,8 +97,21 @@ bool TicTacToe::actionForEmptyHolder(BitHolder *holder)
     //    - Position it at the holder's position (holder->getPosition()).
     //    - Assign it to the holder: holder->setBit(newBit);
 
+
+    //I created a bit & figure out whos turn it is in one line.
+    Bit* newPlace = PieceForPlayer(getCurrentPlayer()->playerNumber());
+    
+    //made a variable for clarity to get Position from holder
+    ImVec2 getPos = holder->getPosition();
+
+    //set position of the bit I created (newPlace)
+    newPlace->setPosition(getPos);
+
+    //assign the bit to given holder.
+    holder->setBit(newPlace);
+
     // 4) Return whether we actually placed a piece. true = acted, false = ignored.
-    return false; // replace with true if you complete a successful placement    
+    return true; // replaced with true when able to place successfully  
 }
 
 bool TicTacToe::canBitMoveFrom(Bit *bit, BitHolder *src)
@@ -102,21 +133,63 @@ void TicTacToe::stopGame()
 {
     // clear out the board
     // loop through the 3x3 array and call destroyBit on each square
+
+     for(int i = 0; i < _gameOptions.rowX; ++i) {
+        for (int j = 0; j < _gameOptions.rowY; ++j) {
+            _grid[j][i].destroyBit();
+        }
+    } 
 }
 
 //
 // helper function for the winner check
-//
+// Note for Alfredo:
+//possibly flip _grid[x][y] if not working?
+
+//ownerAt will return who owns a bit at given index if it exists. (not nullptr)
 Player* TicTacToe::ownerAt(int index ) const
 {
     // index is 0..8, convert to x,y using:
-    // y = index / 3
-    // x = index % 3 
+    int y = index / 3;
+    int x = index % 3;
+   Bit* what = _grid[x][y].bit();    
+   if(!what) return nullptr;
     // if there is no bit at that location (in _grid) return nullptr
     // otherwise return the owner of the bit at that location using getOwner()
-    return nullptr;
+    return what->getOwner();
 }
 
+
+
+
+
+//created to help with clarity and used only in checkForWinner()
+//NOTE: might want to set this to a private helper function instead of a function inside TicTacToe.cpp file
+
+//WHAT IT DOES: this compares a players list and a win_triple list and returns true or false if a player has
+//a winning triple
+bool winnerHelper(std::vector<int> player, std::vector<int> win_triple) {
+     for (int element : win_triple) {
+        if (std::find(player.begin(), player.end(), element) == player.end()) {
+            return false; //Element not found.
+        }
+
+     }
+     return true;
+}
+
+
+
+
+//how I implemented:
+//created  2 ordered list of both players 
+//made it so the winning checks runs past a player list with a size of 3 or greater
+//check in order of which winning triple starts with the lowest index
+// ex) {0,1,2} {0,3,6} {0.4,8}
+// if none is found and a player has this index I pop it out of the list and continue with
+// {1, 4, 7}
+//do this all the way till {6,7,8}.
+// if no winner is found return nullprt
 Player* TicTacToe::checkForWinner()
 {
     // check all the winning triples
@@ -136,17 +209,155 @@ Player* TicTacToe::checkForWinner()
     // if you find a winning triple, return the player who owns that triple
     // otherwise return nullptr
 
+    //Using vectors to get an ordered list on who owns what bit on each indext
+    std::vector<int> player1, player2;
+    for (int i = 0; i <= 8; ++i) {
+        Player* owner = ownerAt(i);
+        if(owner) {
+            if(owner->playerNumber() == 0) {
+                player1.push_back(i);
+            } else {player2.push_back(i);}
+        }
+    }
+    
+    //First check if player size is big enough for a winning tripple which is 3 or greater
+    //only need to check first player on this condition.
+
+    //finally run through each possible winning tripples
+
+    if(player1.size() >= 3) {
+        
+        //declare all lists at this point
+        std::vector<int> win_triple1,win_triple2,win_triple3,win_triple4,win_triple5,win_triple6,win_triple7, win_triple8;
+
+        //check for 0 win_triples first and declare this as follows:
+        win_triple1 = {0, 1, 2};
+        win_triple2 = {0, 3, 6};
+        win_triple3 = {0, 4, 8};
+
+    //since lists are ordered check if & who placed on index 0 and run winnerHelper() on each possible winning list with player list.
+    //these check continues for each winning triple.
+    if(player1.front() == 0) {
+        
+            //player one has any of these 3 triples so return with ownerAt(0)
+
+        if(winnerHelper(player1, win_triple1) || winnerHelper(player1, win_triple2) || winnerHelper(player1, win_triple3)) {
+            return ownerAt(0);
+        }
+        //remove and continue if not found
+        player1.erase(player1.begin());
+
+    } else if(player2.front() == 0) {
+
+         if(winnerHelper(player2, win_triple1) || winnerHelper(player2, win_triple2) || winnerHelper(player2, win_triple3)) {
+            return ownerAt(0);
+        }
+        //remove and continue if not found
+        player2.erase(player2.begin());
+   
+   
+    } 
+    //do the check with {1,4,7}
+    win_triple4 = {1,4,7};
+    if (player1.front() == 1) {
+        if(winnerHelper(player1, win_triple4)) {
+            return ownerAt(1);
+        }
+         //remove and continue if not found
+        player1.erase(player1.begin());
+
+    }   else if (player2.front() == 1) {
+        if(winnerHelper(player2, win_triple4)) {
+            return ownerAt(1);
+        }
+         //remove and continue if not found
+        player2.erase(player2.begin());
+    }
+
+    //do check with {2,5,8}, {2,4,6}
+    win_triple5 = {2,5,8};
+    win_triple6 = {2,4,6};
+
+     if (player1.front() == 2) {
+        if(winnerHelper(player1, win_triple5) || winnerHelper(player1, win_triple6)) {
+            return ownerAt(2);
+        }
+         //remove and continue if not found
+        player1.erase(player1.begin());
+
+    }   else if (player2.front() == 2) {
+        if(winnerHelper(player2, win_triple5) || winnerHelper(player2, win_triple6)) {
+            return ownerAt(2);
+        }
+         //remove and continue if not found
+        player2.erase(player2.begin());
+    }
+
+    //check for {3,4,5}
+    win_triple7 = {3,4,5};
+
+     if (player1.front() == 3) {
+        if(winnerHelper(player1, win_triple7)) {
+            return ownerAt(3);
+        }
+         //remove and continue if not found
+        player1.erase(player1.begin());
+
+    }   else if (player2.front() == 3) {
+        if(winnerHelper(player2, win_triple7)) {
+            return ownerAt(3);
+        }
+         //remove and continue if not found
+        player2.erase(player2.begin());
+    }
+
+    //check for {6,7,8}
+    win_triple8 = {6,7,8};
+
+     if (player1.front() == 6) {
+        if(winnerHelper(player1, win_triple8)) {
+            return ownerAt(6);
+        }
+         
+
+    }   else if (player2.front() == 6) {
+        if(winnerHelper(player2, win_triple8)) {
+            return ownerAt(6);
+        }
+    }
+    }
+
+
+    //if no winning triple is found just return nullptr until found with checkForWinner() or checkForDraw().
+    //
+    
+    return nullptr;
+
     // Hint: Consider using an array to store the winning combinations
     // to avoid repetitive code
-    return nullptr;
 }
 
+
+
+
+
+//How I implemented:
+//only return true if there are no empty squares
+//otherwise it will return true if one is found in the for loop
 bool TicTacToe::checkForDraw()
 {
+
     // is the board full with no winner?
     // if any square is empty, return false
     // otherwise return true
-    return false;
+    for (int i = 0; i <= 8; ++i) {
+        Player* owner = ownerAt(i);
+        if(!owner) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 //
@@ -157,10 +368,23 @@ std::string TicTacToe::initialStateString()
     return "000000000";
 }
 
+
+
+
+
+
+
 //
 // this still needs to be tied into imguis init and shutdown
 // we will read the state string and store it in each turn object
 //
+//How I implemented:
+//create an empty string and loop through _grid[y][x] left-to-right, top-to-bottom
+//if a bit exist on that part of a grid add what player that is and +1 to show if its player 1 or 2.
+// used += and to_string() 
+//if no bit exist then make it 0 with else.
+//one done return that string.
+
 std::string TicTacToe::stateString() const
 {
     // return a string representing the current state of the board
@@ -176,8 +400,31 @@ std::string TicTacToe::stateString() const
     // remember that player numbers are zero-based, so add 1 to get '1' or '2'
     // if the bit is null, add '0' to the string
     // finally, return the constructed string
-    return "000000000";
+    
+   
+    std::string current_state;
+     for(int i = 0; i < _gameOptions.rowX; ++i) {
+        for (int j = 0; j < _gameOptions.rowY; ++j) {
+            if(_grid[j][i].bit()) {
+            
+           current_state += std::to_string(_grid[j][i].bit()->getOwner()->playerNumber() + 1);
+            }
+            else {
+                current_state += "0";
+            }
+        }
+    } 
+    return current_state;
 }
+
+
+
+//not done
+//Implementation so far:
+//itterate through string s, get player number by subtracting 1
+//create a new bit if any player placed on that square prior
+//set position for new bit
+//setbit on holder found with [index / 3] [index % 3]
 
 //
 // this still needs to be tied into imguis init and shutdown
@@ -205,6 +452,23 @@ void TicTacToe::setStateString(const std::string &s)
     // loop through the 3x3 array and set each square accordingly
     // the string should always be valid, so you don't need to check its length or contents
     // but you can assume it will always be 9 characters long and only contain '0', '1', or '2'
+    
+    for(size_t i = 0; i < s.length(); i++) {
+        int playerNumber = s[i] - '0';
+        int y = i / 3;
+        int x = i % 3;
+        if(playerNumber == 1 || playerNumber == 2) {
+         Bit* savePlace = PieceForPlayer(playerNumber - 1);
+         BitHolder*  getHol = _grid[x][y].bit()->getHolder();
+         ImVec2 getPos = getHol->getPosition();
+
+         savePlace->setPosition(getPos);
+         getHol->setBit(savePlace);
+
+        }
+        
+    }
+
 }
 
 
